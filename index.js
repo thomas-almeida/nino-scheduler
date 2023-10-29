e605af45a89c5413069d7ae6d69a605d828fc4fe
thomas-almeida <contato.thomasalmeidard@gmail.com>
2023-10-29 07:43:05 -0300

    .env data

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const TelegramBot = require('node-telegram-bot-api/lib/telegram')
 
-const TK = '6539006442:AAGJ5rEGI9nNrFtehJrZmdwoACjSsVMr5yE'
-const CID = '5992948811'
+require('dotenv').config();
+
+const telegramToken = process.env.TELEGRAM_TOKEN;
+const telegramChatId = process.env.CHAT_ID;
 
 const app = express()
 app.use(cors())
@@ -13,19 +15,19 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 3001
 
-const bot = new TelegramBot(TK, { polling: true })
+const bot = new TelegramBot(telegramToken, { polling: true })
 
 app.get('/nino', (req, res) => {
     res.send('Meaw')
 })
 
 app.post('/send-schedule', (req, res) => {
-    const chatId = CID
+    const chatId = telegramChatId
     const message = req.body.mensagem
 
     bot.sendMessage(chatId, message)
-    .then(() => res.send('Mensagem enviada'))
-    .catch((error) => res.status(500).send('Erro ao enviar' + error))
+        .then(() => res.send('Mensagem enviada'))
+        .catch((error) => res.status(500).send('Erro ao enviar' + error))
 })
 
 app.listen(port, () => {
